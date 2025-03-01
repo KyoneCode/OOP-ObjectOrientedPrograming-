75c5e2c9f8c366fae9fafcfe54016ec841ce96f2
Tio <hadyan.kp@gmail.com>
2025-03-02 00:03:24 +0700

    Update MataKuliah.java

--- pertemuan2/Mahasiswa/MataKuliah.java
+++ pertemuan2/Mahasiswa/MataKuliah.java
@@ -3,22 +3,29 @@
 public class MataKuliah {
     //atribut
     private String idMatKul;
+    private String nama;
     private int sks;
 
     //konstruktor
     public MataKuliah(){
         idMatKul = "";
+        nama = "";
         sks = 0;
     }
 
-    public MataKuliah(String idString, int sks){
+    public MataKuliah(String idString, String nama, int sks){
         this.idMatKul = idMatKul;
+        this.nama = nama;
         this.sks = sks;
     }
 
     //selektor dan mutator
     public String getIdMatKul() {
         return idMatKul;
+    }
+    
+    public String getNama() {
+        return nama;
     }
 
     public int getSks() {
@@ -28,6 +35,11 @@ public class MataKuliah {
     public void setIdMatKul(String x) {
         this.idMatKul = x;
     }
+    
+    public void setNama(String x) {
+        this.nama = x;
+    }
+    
     public void setSks(int x) {
         this.sks = x;
     }
