36b1c3f78a22018a8138a48edb031053b6f2d940
Tio <hadyan.kp@gmail.com>
2025-03-10 19:57:16 +0700

    Create JumlahPesananMelebihiStokException.java

---
+++ pertemuan3/Tugas/Exception/JumlahPesananMelebihiStokException.java
@@ -0,0 +1,4 @@
+public class JumlahPesananMelebihiStokException extends Exception {
+    // TO DO 2: Buat konstructor dengan parameter message dan panggil konstruktor parent dengan keyword super
+    
+}
