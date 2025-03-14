047b10ca4e6e6c029b23086ef78d085722d44ae9
Tio <hadyan.kp@gmail.com>
2025-03-14 17:14:19 +0700

    Create lingkaran.java

---
+++ pertemuan4/Kelas(Modul)/lingkaran.java
@@ -0,0 +1,51 @@
+
+package Java.Pertemuan4;
+
+public class lingkaran extends BangunDatar {
+    //atribut
+    private double jari;
+    private static int counterLingkaran = 0;
+    //method
+    /*konstruktor*/
+    public lingkaran(){
+        jari = 0;
+        jmlSisi = 0;
+        warna = "";
+        border = "";
+        counterLingkaran++;
+    }
+    public lingkaran(double jari, double jmlSisi, String warna, String border){
+        this.jari = jari;
+        this.jmlSisi = jmlSisi;
+        this.warna = warna;
+        this.border = border;
+        counterLingkaran++;
+    }
+    //print counter
+    public double printcounter(){
+        return counterLingkaran;
+    }
+    //setter getter
+    public double getJari() {
+        return jari;
+    }
+
+    public void setJari(double jari) {
+        this.jari = jari;
+    }
+
+    public double getluas(){
+        return Math.PI * getJari() * getJari();
+    }
+
+    public double getkeliling(){
+        return 2 * Math.PI * getJari();
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Jari :"+getJari());
+    }
+}
+
