f51014ceea6ef39b826e0a487c0cafe2ba1a6e90
Tio <hadyan.kp@gmail.com>
2025-03-16 23:34:51 +0700

    Update Tendik.java

--- pertemuan4/Tugas/Tendik.java
+++ pertemuan4/Tugas/Tendik.java
@@ -7,11 +7,19 @@ import java.time.Period;
 public class Tendik extends Pegawai{
     //abstract
     protected String Bidang;
+    protected int BUP = 55;
     //konstruktor
     public Tendik(){}
-    public Tendik(String NIP, String Nama, LocalDate TanggalLahir, String Bidang, LocalDate TMT, String Jabatan, String GajiPokok){
+    public Tendik(String NIP, String Nama, LocalDate TanggalLahir, String Bidang, LocalDate TMT, String Jabatan, String GajiPokok, int BUP){
         super(NIP, Nama, TanggalLahir, TMT, Jabatan, GajiPokok);
         this.Bidang = Bidang;
+        this.BUP = BUP;
+    }
+    public int getBUP() {
+        return BUP;
+    }
+    public void setBUP(int bUP) {
+        BUP = bUP;
     }
     public String getBidang() {
         return Bidang;
@@ -20,7 +28,7 @@ public class Tendik extends Pegawai{
         Bidang = bidang;
     }
     public LocalDate hitungBUP() {
-        LocalDate usiaPensiun = getTanggalLahir().plusYears(55); 
+        LocalDate usiaPensiun = getTanggalLahir().plusYears(BUP); 
         return usiaPensiun.withDayOfMonth(1).plusMonths(1); 
     }
     public double MasaKerjaTahun(){
@@ -44,7 +52,7 @@ public class Tendik extends Pegawai{
         System.out.println("Bidang       : " + Bidang);
         System.out.println("Masa Kerja   : " + hitungMasaKerja());
         System.out.println("BUP          : " + hitungBUP().format(FORMATTER));
-        System.out.println("Gaji Pokok   : " + GajiPokok);
+        System.out.println("Gaji Pokok   : Rp." + GajiPokok+".00");
         System.out.println("Tunjangan    : " + "1% x "+ MasaKerjaTahun() + " x Rp. " + GajiPokok+ " = Rp. "+ Tunjangan()+"0");
     }
 }
