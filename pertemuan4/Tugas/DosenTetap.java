1b6adf42b3b139bd96c174b1afd90fbc0dfb3c0f
Tio <hadyan.kp@gmail.com>
2025-03-16 23:36:16 +0700

    Update DosenTetap.java

--- pertemuan4/Tugas/DosenTetap.java
+++ pertemuan4/Tugas/DosenTetap.java
@@ -7,11 +7,19 @@ import java.time.Period;
 public class DosenTetap extends Dosen {
     //abstract
     protected String NIDN;
+    protected int BUP = 65;
     //konstruktor
     public DosenTetap(){}
-    public DosenTetap(String NIDN, String NIP, String Nama, LocalDate TanggalLahir, String Fakultas, LocalDate TMT, String Jabatan, String GajiPokok){
+    public DosenTetap(String NIDN, String NIP, String Nama, LocalDate TanggalLahir, String Fakultas, LocalDate TMT, String Jabatan, String GajiPokok, int BUP){
         super(NIP, Nama, TanggalLahir, Fakultas, TMT, Jabatan, GajiPokok);
         this.NIDN=NIDN;
+        this.BUP = BUP;
+    }
+    public int getBUP() {
+        return BUP;
+    }
+    public void setBUP(int bUP) {
+        BUP = bUP;
     }
     public String getNIDN() {
         return NIDN;
@@ -20,8 +28,8 @@ public class DosenTetap extends Dosen {
         NIDN = nIDN;
     }
     public LocalDate hitungBUP() {
-        LocalDate usiaPensiun = getTanggalLahir().plusYears(65); // Tambah 60 tahun dari tanggal lahir
-        return usiaPensiun.withDayOfMonth(1).plusMonths(1); // Geser ke tanggal 1 bulan berikutnya
+        LocalDate usiaPensiun = getTanggalLahir().plusYears(BUP); 
+        return usiaPensiun.withDayOfMonth(1).plusMonths(1); 
     }
 
     public double MasaKerjaTahun(){
@@ -45,7 +53,7 @@ public class DosenTetap extends Dosen {
         System.out.println("Fakultas     : " + Fakultas);
         System.out.println("Masa Kerja   : " + hitungMasaKerja());
         System.out.println("BUP          : " + hitungBUP().format(FORMATTER));
-        System.out.println("Gaji Pokok   : " + GajiPokok);
+        System.out.println("Gaji Pokok   : Rp." + GajiPokok+".00");
         System.out.println("Tunjangan    : " + "2% x "+ MasaKerjaTahun() + " x Rp. " + GajiPokok+ " = Rp. "+ Tunjangan()+"0");
     }
 }
