b666d0ab6c27604fc7955692347eec14713a47b2
Tio <hadyan.kp@gmail.com>
2025-03-16 20:58:08 +0700

    Create Dosen.java

---
+++ pertemuan4/Tugas/Dosen.java
@@ -0,0 +1,27 @@
+package Pertemuan4.Tugas;
+
+import java.time.LocalDate;
+
+public abstract class Dosen extends Pegawai {
+    //abstract
+    protected String Fakultas;
+    //konstruktor
+    public Dosen(){}
+    public Dosen(String NIP, String Nama, LocalDate TanggalLahir, String Fakultas, LocalDate TMT, String Jabatan, String GajiPokok){
+        super(NIP, Nama, TanggalLahir, TMT, Jabatan, GajiPokok);
+        this.Fakultas=Fakultas;
+    }
+    public String getFakultas() {
+        return Fakultas;
+    }
+    public void setFakultas(String fakultas) {
+        Fakultas = fakultas;
+    }
+    @Override
+    public void printInfo() {
+        // TODO Auto-generated method stub
+        super.printInfo();
+        System.out.println("Fakultas     : " + Fakultas);
+    }
+
+}
