c7cc111ad042ba45bbd92a5a8eae7758500ed6c0
Tio <hadyan.kp@gmail.com>
2025-03-24 21:07:51 +0700

    Update MManusia.java

--- pertemuan5/Tugas/MManusia.java
+++ pertemuan5/Tugas/MManusia.java
@@ -7,18 +7,18 @@ public class MManusia {
         Petani pt1 = new Petani("Nugraha", LocalDate.of(1977, 1, 9), "Jl. Bunga 9 Tembalang", 5000000, "Wonogiri");
         PNS p2 = new PNS("Panji", LocalDate.of(2010, 4, 1), "Jl. Panorama 111 Tembalang", 10000000, "198004212010041002");
 
-        // System.out.println("Jumlah Manusia    = " + Manusia.getCounterMns());
-        // System.out.println("Jumlah PNS        = " + PNS.getCounterPNS());
-        // System.out.println("Jumlah Pengusaha  = " + Pengusaha.getCounterPengusaha());
-        // System.out.println("Jumlah Petani     = " + Petani.getCounterPetani());
+        System.out.println("Jumlah Manusia    = " + Manusia.getCounterMns());
+        System.out.println("Jumlah PNS        = " + PNS.getCounterPNS());
+        System.out.println("Jumlah Pengusaha  = " + Pengusaha.getCounterPengusaha());
+        System.out.println("Jumlah Petani     = " + Petani.getCounterPetani());
 
-        // System.out.println("Pajak PNS p1         = Rp " + p1.hitungPajak());
-        // System.out.println("Pajak Pengusaha pe1  = Rp " + pe1.hitungPajak());
-        // System.out.println("Pajak Petani pt1     = Rp " + pt1.hitungPajak());
+        System.out.println("Pajak PNS p1         = Rp " + p1.hitungPajak());
+        System.out.println("Pajak Pengusaha pe1  = Rp " + pe1.hitungPajak());
+        System.out.println("Pajak Petani pt1     = Rp " + pt1.hitungPajak());
 
-        // System.out.println("Masa Kerja p1  = " + p1.hitungMasaKerja());
-        // System.out.println("Masa Kerja pe1 = " + pe1.hitungMasaKerja());
-        // System.out.println("Masa Kerja pt1 = " + pt1.hitungMasaKerja());
+        System.out.println("Masa Kerja p1  = " + p1.hitungMasaKerja());
+        System.out.println("Masa Kerja pe1 = " + pe1.hitungMasaKerja());
+        System.out.println("Masa Kerja pt1 = " + pt1.hitungMasaKerja());
 
         p1.cetakInfo();
         pe1.cetakInfo();
