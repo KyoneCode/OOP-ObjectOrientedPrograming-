2907051d90ee2d8ff4eaec5e30d47c77b2c0f8b9
Tio <hadyan.kp@gmail.com>
2025-03-24 21:16:56 +0700

    Update BangunMain.java

--- pertemuan5/Modul/BangunMain.java
+++ pertemuan5/Modul/BangunMain.java
@@ -1,4 +1,5 @@
 package pertemuan5.Modul;
+
 public class BangunMain {
     public static void main(String[] args) {
         BangunDatar P1 = new Persegi(2, 4, "Merah", "Putih");
@@ -8,8 +9,14 @@ public class BangunMain {
         L1.printInfo();
         boolean isEqual = P1.isEqualLuas(L1);
         System.out.println("Luas sama: " + isEqual);
-
-
+        System.err.println("**************************************************");
+        ((Persegi)P1).zoomIn();
+        System.out.println("Luas Persegi setelah zoom in : "+P1.getluas());
+        ((Persegi)P1).zoomOut();
+        System.out.println("Luas Persegi setelah zoom out : "+P1.getluas());
+        ((Persegi)P1).zoom(10);
+        System.out.println("Luas Persegi setelah zoom 10% : "+P1.getluas());
 
     }   
 }
+
