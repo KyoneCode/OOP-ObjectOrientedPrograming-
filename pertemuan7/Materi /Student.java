c60fe6b4069d6fed6efe4cc5a486f80238e027e9
Tio <hadyan.kp@gmail.com>
2025-04-24 08:16:16 +0000

    nambahin

---
+++ pertemuan7/Materi /Student.java
@@ -0,0 +1,9 @@
+package pertemuan7.Materi;
+// Student.java
+public class Student extends Person {
+    public Student(String name) {
+        super(name);
+    }
+    @Override
+    public boolean isAsleep(int hr) { return 2 < hr && 8 > hr; }
+}
---
+++ pertemuan7/Materi /TestStatus.java
@@ -0,0 +1,9 @@
+package pertemuan7.Materi;
+// TestStatus.java
+public class TestStatus {
+    public static void main(String[] args) {
+        Person p;
+        p = new Student("Sally");
+        p.status(1);
+    }
+}
