76b2b8c95ba416d306295d03386e6b18cfc5ed02
Tio <hadyan.kp@gmail.com>
2025-05-02 08:38:18 +0700

    Create Senjata.java

---
+++ pertemuan8/Tugas/VeryEasy/Senjata.java
@@ -0,0 +1,29 @@
+public class Senjata {
+    private String bunyi;
+    private boolean menusuk;
+    private int peluru;
+    //konstruct
+    public Senjata(String bunyi){
+        this.bunyi=bunyi;;
+        this.peluru = 0;
+    }
+    //metode
+    public void setBunyi(String bunyi){
+    this.bunyi = bunyi;   
+    }
+    public String getBunyi(){
+        return this.bunyi;
+    }
+    public void setPeluru(int peluru){
+        this.peluru = peluru;
+    }
+    public int getPeluru(){
+        return peluru;
+    }
+        public void menembak(int x){
+        for(int i = 1; i <=x && i <= peluru; i++){
+            System.out.println(getBunyi());
+            peluru = peluru - 1;
+        }
+    }
+}
